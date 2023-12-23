d24d38f17602aace85f0cf848bd52b1f5b84fd28
nizamum <nizamum@code.edu.az>
2023-12-23 15:05:11 +0400

    feat: Array methods

---
+++ task/script1.js
@@ -0,0 +1,83 @@
+// const users = [
+// {
+//     id:10,
+//     age: 24,
+//     surname:'Maharramov',
+//     name:'Nizam',
+//     address: {
+//         street: 'Bulbul 22',
+//         city: 'Baku',
+//         country: 'Azerbaijan'
+//     }
+// },
+// {
+//     id: 2,
+//     name: 'Khayal',
+//     surname: 'Hatamov',
+//     age: 45,
+//     address: {
+//         street: 'Bulbul 23',
+//         city: 'Baku',
+//         country: 'Azerbaijan'
+//     }
+// }, {
+//     id: 3,
+//     name: 'Khay',
+//     surname: 'Hatam',
+//     age: 28,
+//     address: {
+//         street: 'Bulbul 24',
+//         city: 'Baku',
+//         country: 'Azerbaijan'
+//     },
+// },
+// ]
+
+// // // console.log(users[0].surname);
+// // // let sum = users.reduce((acc,curr) => acc + curr.users)
+// // // console.log(sum);
+
+// // users.sort((a, b) => a.age - b.age);
+// // users.sort((a, b) => b.age - a.age);
+
+// users.reverse();
+// console.log(users);
+
+// let result1 = people.filter((users) => users.name.length >= 6);
+// console.log(result1);
+
+// let result2 = users.find((users) => users.age > 25);
+// console.log(result2);
+
+// let result3 = people.map(users => 'myAge ' + users.age);
+// console.log(result3);
+
+
+// let user='nizam';
+
+// let result1=user.slice(1,-1).toUpperCase()+user.slice(-1).toUpperCase();
+// console.log(result1);
+
+// let result2=user.slice(0,2).toLowerCase()+user.slice(2,3).toUpperCase()+user.slice(-2).toLowerCase();
+// console.log(result2);
+
+// let result3=user.slice(0,-1).toLowerCase()+user.slice(-1).toUpperCase();
+// console.log(result3);
+
+// let user1='ulfat zakirli';
+
+// let result4=user1.slice(0,1).toUpperCase()+user1.slice(1,4).toLowerCase()+user1.slice(6,7).toUpperCase()+user1.slice(7,12).toLowerCase()+user1.slice(-1).toLocaleLowerCase();
+// console.log(result4);
+
+// let user = 'ulfat zakirli'
+
+// const result = user.split(' ').map(item => item.slice(0,1).toUpperCase() + item.slice(1)).join(' ')
+
+// console.log(result);
+
+// const studentArr = ['Yusif', 'Rashad', 'Nihad', 'Nezire', 'Metin', 'Heyder', 'Rafiq','Eli', 'Nizam', 'Xeyale', 'Abbas', 'Anar', 'Ulfat']
+// const result = studentArr.map
+
+
+
+console.log('Hi');
